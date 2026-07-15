ebe815429ef3af600eb8e5812e7e59ef3848d351
jestemkojak <827279+jestemkojak@users.noreply.github.com>
2026-07-15 21:12:10 +0200

    feat(usb-msc): expose read-path diagnostics

--- gateware/src/top/sid/top.py
+++ gateware/src/top/sid/top.py
@@ -716,6 +716,9 @@ class SIDSoc(TiliquaSoc):
                     self.usb_msc.reject_txdone_i.eq(msc.reject_txdone),
                     self.usb_msc.nyet_count_i.eq(msc.nyets),
                     self.usb_msc.phase_i.eq(msc.phase_o),
+                    self.usb_msc.engine_rx_bytes_i.eq(msc.rx_bytes_o),
+                    self.usb_msc.engine_stream_mode_i.eq(msc.stream_mode_o),
+                    self.usb_msc.engine_data_len_512_i.eq(msc.data_len_512_o),
                     self.usb_msc.speed_i.eq(msc.speed_o),
                 ]
 
